42c4bc9accd6f1eb3006e7d30d3fb152d1f665d9
ci <ci@hail.is>
2024-07-10 13:07:08 +0000

    setup repo

---
+++ ci/ci/constants.py
@@ -0,0 +1,32 @@
+import os
+from typing import List, Optional
+
+GITHUB_CLONE_URL = 'https://github.com/'
+GITHUB_STATUS_CONTEXT = os.environ["HAIL_CI_GITHUB_CONTEXT"]
+SERVICES_TEAM = 'Services'
+COMPILER_TEAM = 'Compiler'
+TEAMS = [SERVICES_TEAM, COMPILER_TEAM]
+
+
+class User:
+    def __init__(self, gh_username: str, hail_username: Optional[str] = None, teams: Optional[List[str]] = None):
+        self.gh_username = gh_username
+        self.hail_username = hail_username
+        self.teams = teams if teams is not None else []
+
+
+AUTHORIZED_USERS = [
+    User('bw2'),
+    User('chrisvittal', 'cvittal', [COMPILER_TEAM]),
+    User('cseed', 'cseed'),
+    User('daniel-goldstein', 'dgoldste', [SERVICES_TEAM]),
+    User('jigold', 'jigold'),
+    User('jkgoodrich', 'jgoodric'),
+    User('konradjk', 'konradk'),
+    User('nawatts'),
+    User('patrick-schultz', 'pschultz', [COMPILER_TEAM]),
+    User('illusional', 'mfrankli'),
+    User('iris-garden', 'irademac'),
+    User('ehigham', 'ehigham', [COMPILER_TEAM]),
+    User('sjparsa', 'parsa'),
+]
---
+++ hail/python/hail/docs/_static/hail_version.js
@@ -0,0 +1,1 @@
+hail_version="0.2.132-61e0a3168cfa";hail_pip_version="0.2.132"
